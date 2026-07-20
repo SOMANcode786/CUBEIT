193b6cd68c3adf03259cc2342d9c71a1b88cf94a
Muhammad Soman <somanamir43@gmail.com>
2026-07-21 01:24:52 +0500

    up

--- src/components/client-work-showcase.tsx
+++ src/components/client-work-showcase.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import Image from "next/image";
-import { ArrowLeft, ArrowRight, ArrowUpRight, Briefcase } from "lucide-react";
+import { ArrowLeft, ArrowRight, ArrowUpRight } from "lucide-react";
 import {
   AnimatePresence,
   motion,
